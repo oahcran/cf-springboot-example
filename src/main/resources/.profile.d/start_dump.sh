4344fa318c275e4171bdbd105693046b0dacb20a
Chao Ran <oahcran@users.noreply.github.com>
2017-03-17 13:49:59 -0500

    Update start_dump.sh

--- src/main/resources/.profile.d/start_dump.sh
+++ src/main/resources/.profile.d/start_dump.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-chmod 755 $HOME/dump.sh
-$HOME/dump.sh &
+
+if [ -f $HOME/app/BOOT-INF/classes/dump.sh ];
+then
+  export DUMP_SH_FILE=$HOME/app/BOOT-INF/classes/dump.sh
+else
+  export DUMP_SH_FILE=$HOME/dump.sh
+fi
+
+chmod 755 $DUMP_SH_FILE
+$DUMP_SH_FILE &
